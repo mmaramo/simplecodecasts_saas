13cd09eb3a869744da9348930ad879f6eefc4573
Mark Maramo <mark.maramo2008@gmail.com>
2017-10-20 14:22:38 +0000

    Edited layout and routes for contact page and added contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/contact' => 'pages#contact'
+  resources :contacts
+  get '/contactus' => 'pages#contactus'
   get '/about' => 'pages#about'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
