967cf5ed7694110b10c3b09a53562dc2b1889969
Mark Maramo <mark.maramo2008@gmail.com>
2017-10-23 07:18:57 +0000

    Added heading in the form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,21 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            flash[:success] = "You have signed up successfully!"
+            redirect_to new_contact_path
+        else 
+            flash[:error] = "An error occured. Please try again!"
+            redirect to new_contact_path
+        end 
+        
     end
+    
+    private
+    
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
